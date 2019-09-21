f5ced646766103e760235435de55a15dc2097854
nils <nt@konfidas.de>
2019-09-21 21:27:39 +0200

    Before update of npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-var webpack = require('webpack');
-var fileName = 'pdf-annotate';
+var webpack = require("webpack");
+var fileName = "pdf-annotate";
 var plugins = [];
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 if (process.env.MINIFY) {
-  fileName += '.min'
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
+  fileName += ".min";
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: plugins,
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    filename: 'dist/' + fileName + '.js',
-    library: 'PDFAnnotate',
-    libraryTarget: 'umd'
+    filename: "dist/" + fileName + ".js",
+    library: "PDFAnnotate",
+    libraryTarget: "umd"
   },
+
+  plugins: [new HtmlWebpackPlugin({title: "Output Management"})],
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015'],
-          plugins: ['add-module-exports']
+          presets: ["es2015"],
+          plugins: ["add-module-exports"]
         }
       }
     ]
