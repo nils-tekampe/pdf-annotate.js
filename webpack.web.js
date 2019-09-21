ba8248e3c2c67452d168518c38dfc729901a298a
nils <nt@konfidas.de>
2019-09-22 01:36:44 +0200

    working

--- webpack.web.js
+++ webpack.web.js
@@ -2,11 +2,11 @@ var webpack = require("webpack");
 
 module.exports = {
   entry: "./docs/main.js",
-
   output: {
     filename: "index.js",
     path: "/Users/nils/Documents/gitRepositories/pdf-annotate.js/docs"
   },
+  devtool: "source-map",
 
   module: {
     rules: [
